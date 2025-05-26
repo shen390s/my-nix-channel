22614307cf1e1a3443bdd8994a4ca1d046736d61
Rongsong Shen <rshen@nixos.akrostar-tech.com>
2025-05-26 08:31:35 +0000

    improve pkgs

--- default.nix
+++ default.nix
@@ -1,7 +1,6 @@
 # default.nix
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  pkgs = import <nixpkgs> { config = {}; overlays = []; };
 in
 {
   xbuild = pkgs.callPackage ./pkgs/x/xbuild/package.nix {};
