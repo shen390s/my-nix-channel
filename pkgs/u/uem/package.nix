025abd1480a9e33d8c931616e6adbf6f95ce3fac
Rongsong Shen <rongsong.shen@gmail.com>
2025-11-15 15:26:03 +0800

    save

--- pkgs/u/uem/package.nix
+++ pkgs/u/uem/package.nix
@@ -27,29 +27,45 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-WLQak+5dbD4Zo3EgnW5YVhj5tNPJlkXYdd4lNLzRlq0=";
   };
 
-  nativeBuildInputs = [ pkgs.roswell ];
-  doCheck = true;
+  buildInputs = with pkgs; [ 
+    roswell 
+    patchelf
+    zstd
+#    asdf-vm 
+  ];
+
+  propagatedBuildInputs = with pkgs; [
+    zstd
+  ];
+
+  doCheck = true; 
 
   configurePhase = ''
-     mkdir -p .roswell
-     cat >.roswell/init.lisp <<EOF
-     (format nil "Hell,world~%")
-     ;;(push "$src" asdf:*central-registry*)
-     EOF
+    true
   '';
   
   buildPhase = ''
      set +e
      set -x
-     cat .roswell/init.lisp
+     export ROSWELL_HOME=$PWD/.roswell
+     export HOME=$TMP
      ros setup
-     # find /build/.roswell
-     # echo running
-     sh -x ros version
-     find .
-     find /build/source
-     # ros build $src/roswell/uem.ros
-     mkdir -p $out
-     cp .roswell/init.lisp $out
+     ros use sbcl-bin/system
+     export ASDF_CENTRAL_REGISTRY="$src"
+     cat >$ROSWELL_HOME/init.lisp <<EOF
+       (ros:ensure-asdf)
+       (push (uiop:ensure-directory-pathname "$src/") asdf:*central-registry*)
+     EOF
+     cat $ROSWELL_HOME/init.lisp
+     cp -Rf $src/roswell .
+     ros build  roswell/uem.ros --disable-compression 
   '';          
+
+  installPhase = ''
+     mkdir -p $out/bin
+     cp roswell/uem $out/bin
+     SBCL_LIB_PATH=$(find $ROSWELL_HOME -type d -name "lib")
+     ZSTD_LIB_PATH=${pkgs.zstd}/lib
+     patchelf --set-rpath "$SBCL_LIB_PATH:$ZSTD_LIB_PATH:$out/bin" $out/bin/uem 
+  '';
 })
