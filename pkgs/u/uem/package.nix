e887f0e3d4f9fa5fc56129e997c45cb2a5a79e1f
Rongsong Shen <rongsong.shen@gmail.com>
2025-11-16 16:46:31 +0800

    uem

--- pkgs/u/uem/package.nix
+++ pkgs/u/uem/package.nix
@@ -30,8 +30,8 @@ stdenv.mkDerivation (finalAttrs: {
   buildInputs = with pkgs; [ 
     roswell 
     patchelf
+    sbcl
     zstd
-#    asdf-vm 
   ];
 
   propagatedBuildInputs = with pkgs; [
@@ -39,33 +39,29 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   doCheck = true; 
+  dontStrip = true;
 
   configurePhase = ''
-    true
+    export ROSWELL_HOME=$PWD/.roswell
+    export HOME=$TMP
+    ros setup
+    ros use sbcl-bin/system
   '';
   
   buildPhase = ''
      set +e
      set -x
-     export ROSWELL_HOME=$PWD/.roswell
-     export HOME=$TMP
-     ros setup
-     ros use sbcl-bin/system
-     export ASDF_CENTRAL_REGISTRY="$src"
      cat >$ROSWELL_HOME/init.lisp <<EOF
        (ros:ensure-asdf)
        (push (uiop:ensure-directory-pathname "$src/") asdf:*central-registry*)
      EOF
      cat $ROSWELL_HOME/init.lisp
      cp -Rf $src/roswell .
-     ros build  roswell/uem.ros --disable-compression 
+     ros build  roswell/uem.ros 
   '';          
 
   installPhase = ''
      mkdir -p $out/bin
      cp roswell/uem $out/bin
-     SBCL_LIB_PATH=$(find $ROSWELL_HOME -type d -name "lib")
-     ZSTD_LIB_PATH=${pkgs.zstd}/lib
-     patchelf --set-rpath "$SBCL_LIB_PATH:$ZSTD_LIB_PATH:$out/bin" $out/bin/uem 
   '';
 })
