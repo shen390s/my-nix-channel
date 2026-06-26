efa8b2a3219c0cb2da1be22aee1e4bc56069c5d0
Rongsong Shen <rshen@shenrs.eu>
2026-06-26 19:35:53 +0800

    fix jvm crash of tla-toolbox

--- pkgs/t/tla-toolbox/package.nix
+++ pkgs/t/tla-toolbox/package.nix
@@ -24,7 +24,35 @@ in stdenv.mkDerivation {
     runHook preInstall
     mkdir -p $out/{lib/tla-toolbox,bin}
     cp -r --no-preserve=mode . $out/lib/tla-toolbox/
-    chmod +x $out/lib/tla-toolbox/toolbox
+    find $out/lib/tla-toolbox -type f -exec sh -c 'file "$1" | grep -q "ELF" && chmod +x "$1"' _ {} \;
+
+    # Use system JDK instead of bundled JRE
+    cat > $out/lib/tla-toolbox/toolbox.ini <<EOF
+    -startup
+    plugins/org.eclipse.equinox.launcher_1.6.500.v20230717-2134.jar
+    --launcher.library
+    plugins/org.eclipse.equinox.launcher.gtk.linux.x86_64_1.2.700.v20221108-1024
+    --launcher.defaultAction
+    openFile
+    -name
+    TLA+ Toolbox
+    --launcher.GTK_version
+    3
+    -vm
+    ${jdk17}/bin/java
+    -vmargs
+    -XX:+IgnoreUnrecognizedVMOptions
+    -Xmx1024m
+    -XX:+UseParallelGC
+    -Dorg.eclipse.equinox.http.jetty.http.port=10996
+    -Dosgi.splashPath=platform:/base/
+    -Dosgi.requiredJavaVersion=17.0
+    -Dosgi.instance.area.default=@user.home/.tlaplus/
+    -Dosgi.clean=true
+    EOF
+    # Remove leading whitespace from heredoc
+    sed -i 's/^    //' $out/lib/tla-toolbox/toolbox.ini
+
     makeWrapper $out/lib/tla-toolbox/toolbox $out/bin/tla-toolbox \
       --prefix PATH : ${jdk17}/bin \
       --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ gtk3 glib webkitgtk_4_1 libx11 libxtst libsecret ]}
