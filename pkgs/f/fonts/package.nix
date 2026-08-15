fe017445819ef9b38d98394e5621c2b0b08e48d2
Rongsong Shen <rshen@shenrs.eu>
2026-08-15 19:09:15 +0800

    fonts: switch source to vcs.shenrs.eu

--- pkgs/f/fonts/package.nix
+++ pkgs/f/fonts/package.nix
@@ -1,61 +1,37 @@
 {
   lib,
   stdenv,
-#  fetchFromGitea,
-#  fetchFromGitHub,
-#  fetchgit,
-  pkgs? import <nixpkgs> {},
-  pkg-config,
+  fetchurl,
 }:
 
-let
-  meta =  with lib; {
-    description = "Some true type fonts";
-    homepage = "https://github.com/shen390s/my-ttf-fonts";
-    license = lib.licenses.asl20;
-    platforms = lib.platforms.unix;
-  };
+stdenv.mkDerivation rec {
   pname = "my-ttf-fonts";
   version = "1.0";
-  mysource = pkgs.runCommand "download-fonts" {
-    nativeBuiltInputs = [ pkgs.curl ];
-    outputHash = "sha256-JMWxG0lCkzXhuBoBtEk8N0g6qrLRJo0WGki9A+yTbhU=";
-    outputHashAlgo = "sha256";
-    outputHashMode = "flat";
-  } ''
-     ${pkgs.curl}/bin/curl --output $out  \
-          --location \
-          --max-redirs 20 \
-          --retry 3  \
-          --disable-epsv \
-          --cookie-jar cookies \
-          --insecure \
-          --user-agent curl/8.17.0  -C - --fail \
-          https://gitee.com/shen390s/my-ttf-fonts/archive/refs/tags/v1.0.tar.gz
-  '';
-in
-stdenv.mkDerivation (finalAttrs: {
-  pname = pname;
-  version = version;
-                 
-  src = mysource;
-  
+
+  src = fetchurl {
+    url = "https://vcs.shenrs.eu/rshen/fonts/archive/v${version}.tar.gz";
+    hash = "sha256-X3jlNjHdVugQ2Loj16juEi+TLlirLk3APlW9Oc8ZRZk=";
+  };
+
   unpackPhase = ''
-     tar -zxvf $src
+    tar -zxf $src
   '';
-  
+
+  dontBuild = true;
+
   installPhase = ''
-     mkdir -p $out/share/fonts/truetype
-     _fontdir="${pname}-v${version}"
-     if [ -d $_fontdir/fonts/truetype ]; then
-        ls $_fontdir/fonts/truetype/*
-        cp -r $_fontdir/fonts/truetype/*.ttf $out/share/fonts/truetype/
-	      cp -r $_fontdir/fonts/truetype/*.ttc $out/share/fonts/truetype/
-     else
-        echo "No fonts found in $_fontdir/fonts/truetype"
-        exit 0
-     fi
+    mkdir -p $out/share/fonts/truetype
+    _fontdir="fonts"
+    if [ -d $_fontdir/fonts/truetype ]; then
+      cp -r $_fontdir/fonts/truetype/*.ttf $out/share/fonts/truetype/ 2>/dev/null || true
+      cp -r $_fontdir/fonts/truetype/*.ttc $out/share/fonts/truetype/ 2>/dev/null || true
+    fi
   '';
-  
-  inherit meta;
-})
+
+  meta = with lib; {
+    description = "Some true type fonts";
+    homepage = "https://vcs.shenrs.eu/rshen/fonts";
+    license = licenses.bsd3;
+    platforms = platforms.unix;
+  };
+}
